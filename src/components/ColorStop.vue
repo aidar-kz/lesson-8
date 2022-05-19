138d98581559da8305cf874763893ff9f5be4bc1
Aydeke <aydeke2@gmail.com>
2022-05-19 22:01:06 +0600

    Implement color lock

--- src/components/ColorStop.vue
+++ src/components/ColorStop.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="d-flex mb-1">
     <div>
-      <button>
+      <button @click="$emit('up', index)" :disabled="index === 0">
         <i class="bi bi-arrow-up-short"></i>
       </button>
 
-      <button>
+      <button
+        @click="$emit('down', index)"
+        :disabled="index === colors.length - 1"
+      >
         <i class="bi bi-arrow-down-short"></i>
       </button>
     </div>
@@ -14,13 +17,20 @@
       type="text"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
+      :disabled="color.disabled"
     />
 
     <input
       type="color"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
+      :disabled="color.disabled"
     />
+
+    <button @click="$emit('lockColor', index)">
+      <i v-if="color.disabled" class="bi bi-lock"></i>
+      <i v-else class="bi bi-unlock"></i>
+    </button>
   </div>
 </template>
 
@@ -28,6 +38,9 @@
 export default {
   props: {
     modelValue: String,
+    index: Number,
+    color: Object,
+    colors: Array,
   },
 };
 </script>
--- src/views/GradientView.vue
+++ src/views/GradientView.vue
@@ -1,6 +1,22 @@
 <template>
-  <div style="height: 100vh" :style="{ background: gradient }">
+  <div style="height: 100vh" :style="{ background: radialGradient }">
     <div class="container p-4">
+      <div class="d-flex justify-content-between mb-3" style="max-width: 300px">
+        <div>
+          <button class="btn btn-primary" @click="addColor">
+            <i class="bi bi-plus lg"></i>
+          </button>
+
+          <button class="btn btn-primary ms-1" @click="removeColor">
+            <i class="bi bi-dash lg"></i>
+          </button>
+        </div>
+
+        <button class="btn btn-primary" @click="generateGradient">
+          Рандом
+        </button>
+      </div>
+
       <ColorStop
         v-for="(color, index) in colors"
         :key="color.id"
@@ -8,6 +24,9 @@
         :color="color"
         :index="index"
         :colors="colors"
+        @up="moveUp"
+        @down="moveDown"
+        @lock-color="lockColor"
       />
     </div>
   </div>
@@ -23,7 +42,56 @@ export default {
       { id: 0, hex: "#ff0000", disabled: false },
       { id: 1, hex: "#00ff00", disabled: false },
     ],
+    id: 2,
   }),
+
+  methods: {
+    addColor() {
+      this.colors.push({ id: this.id, hex: this.randomHex(), disabled: false });
+      this.id++;
+    },
+
+    removeColor() {
+      if (this.colors.length > 2) {
+        this.colors.pop();
+      }
+    },
+
+    generateGradient() {
+      for (let i = 0; i < this.colors.length; i++) {
+        if (this.colors[i].disabled === false) {
+          this.colors[i].hex = this.randomHex();
+        }
+      }
+    },
+
+    randomHex() {
+      return "#" + Math.random().toString(16).slice(2, 8);
+    },
+
+    moveUp(index) {
+      if (index > 0) {
+        const temp = this.colors[index];
+        this.colors[index] = this.colors[index - 1];
+        this.colors[index - 1] = temp;
+      }
+    },
+
+    moveDown(index) {
+      if (index < this.colors.length - 1) {
+        const temp = this.colors[index];
+        this.colors[index] = this.colors[index + 1];
+        this.colors[index + 1] = temp;
+      }
+    },
+
+    lockColor(index) {
+      this.colors[index].disabled === true
+        ? (this.colors[index].disabled = false)
+        : (this.colors[index].disabled = true);
+    },
+  },
+
   computed: {
     gradient() {
       let linearGradient = "linear-gradient(45deg";
@@ -33,6 +101,16 @@ export default {
       linearGradient += ")";
       console.log(linearGradient);
       return linearGradient;
+    },
+
+    radialGradient() {
+      let radialGradient = "radial-gradient(circle at center";
+      this.colors.forEach((color) => {
+        radialGradient += `, ${color.hex}`;
+      });
+      radialGradient += ")";
+      console.log(radialGradient);
+      return radialGradient;
     },
   },
 };
